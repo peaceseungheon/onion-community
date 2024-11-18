ecd34461633f18aafa9bbb8ea8de72a70a0f881a
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-11-18 17:46:01 +0900

    feat: 회원 탈퇴 기능 개발

--- backend/src/main/java/com/onion/backend/controller/UserController.java
+++ backend/src/main/java/com/onion/backend/controller/UserController.java
@@ -2,8 +2,11 @@ package com.onion.backend.controller;
 
 import com.onion.backend.entity.User;
 import com.onion.backend.service.UserService;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,9 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-    @PostMapping("/create")
-    public ResponseEntity<User> createUser(@RequestParam("name") String name, @RequestParam("password") String password, @RequestParam("email") String email){
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestParam("name") String name,
+        @RequestParam("password") String password, @RequestParam("email") String email) {
         return ResponseEntity.ok(userService.createUser(name, password, email));
+    }
+
+    @DeleteMapping("{userId}")
+    public ResponseEntity<Void> deleteUser(
+        @Parameter(description = "유저 엔티티의 PK", required = true) @PathVariable("userId") Long userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- backend/src/main/java/com/onion/backend/service/UserService.java
+++ backend/src/main/java/com/onion/backend/service/UserService.java
@@ -19,4 +19,8 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public void deleteUser(Long userId){
+        userRepository.deleteById(userId);
+    }
+
 }
