b37d77dd3d6342f69f63eef5c653e24bd043207d
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-11-18 17:10:13 +0900

    feat: 유저 엔티티 생성

---
+++ backend/src/main/java/com/onion/backend/entity/User.java
@@ -0,0 +1,37 @@
+package com.onion.backend.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "last_login_at")
+    private LocalDateTime lastLoginAt;
+
+}
