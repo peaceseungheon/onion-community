d4766d16c25dffdba4373416f188c13129a21c24
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-11-23 22:48:08 +0900

    feat: 로그인 및 spring security 인증 기능 개발

--- backend/src/main/java/com/onion/backend/controller/UserController.java
+++ backend/src/main/java/com/onion/backend/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.onion.backend.controller;
 
+import com.onion.backend.dto.SignInUser;
+import com.onion.backend.dto.SignUpUser;
 import com.onion.backend.entity.User;
 import com.onion.backend.service.UserService;
 import io.swagger.v3.oas.annotations.Parameter;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +25,14 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestParam("name") String name,
-        @RequestParam("password") String password, @RequestParam("email") String email) {
-        return ResponseEntity.ok(userService.createUser(name, password, email));
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers(){
+        return ResponseEntity.ok(userService.getUsers());
+    }
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<User> createUser(@RequestBody SignUpUser signUpUser) {
+        return ResponseEntity.ok(userService.createUser(signUpUser));
     }
 
     @DeleteMapping("{userId}")
@@ -30,6 +40,19 @@ public class UserController {
         @Parameter(description = "유저 엔티티의 PK", required = true) @PathVariable("userId") Long userId) {
         userService.deleteUser(userId);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody SignInUser signInUser){
+        return ResponseEntity.ok(userService.login(signInUser));
+    }
+
+    @GetMapping("/token/validation")
+    public ResponseEntity<Void> jwtValidate(@RequestParam("token") String token){
+        if(userService.validateToken(token)){
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
     }
 
 }
--- backend/src/main/java/com/onion/backend/service/UserService.java
+++ backend/src/main/java/com/onion/backend/service/UserService.java
@@ -1,8 +1,18 @@
 package com.onion.backend.service;
 
+import com.onion.backend.dto.SignInUser;
+import com.onion.backend.dto.SignUpUser;
 import com.onion.backend.entity.User;
+import com.onion.backend.jwt.JwtUtils;
 import com.onion.backend.repository.UserRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,12 +20,20 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private UserDetailsService userDetailsService;
+    private final JwtUtils jwtUtils;
 
-    public User createUser(String name, String password, String email){
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User createUser(SignUpUser signUpUser){
         User user = new User();
-        user.setName(name);
-        user.setPassword(password);
-        user.setEmail(email);
+        user.setName(signUpUser.getName());
+        user.setPassword(passwordEncoder.encode(signUpUser.getPassword()));
+        user.setEmail(signUpUser.getEmail());
         return userRepository.save(user);
     }
 
@@ -23,4 +41,14 @@ public class UserService {
         userRepository.deleteById(userId);
     }
 
+    public String login(SignInUser signInUser) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+            signInUser.getUserId(), signInUser.getPassword());
+        Authentication authenticate = authenticationManager.authenticate(token);
+        return jwtUtils.generateToken(authenticate.getName());
+    }
+
+    public boolean validateToken(String token) {
+        return jwtUtils.validateToken(token);
+    }
 }
