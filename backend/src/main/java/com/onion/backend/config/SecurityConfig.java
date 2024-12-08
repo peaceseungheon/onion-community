e9eecc176e0de858a4433676d7413723186c6689
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-12-09 00:51:06 +0900

    config: preflight 요청에 대한 허가 설정

--- backend/src/main/java/com/onion/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/onion/backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.onion.backend.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,6 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(AbstractHttpConfigurer::disable)
             // CSRF 설정: 필요 시 활성화 (기본적으로 활성화됨)
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(httpSecuritySessionManagementConfigurer -> {
@@ -32,9 +34,10 @@ public class SecurityConfig {
             })
             // 요청 URL 권한 설정
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 // Swagger UI 및 API 문서 경로 허용
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/users/sign-up",
-                    "/api/users/login").permitAll()
+                    "/api/users/login", "/api/boards").permitAll()
                 // 나머지 요청은 인증 필요
                 .anyRequest().authenticated()
             ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
