fdc3dcd535f51d7a531c99285bf11b7ec1440bb8
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-12-31 00:51:05 +0900

    chore: 채팅 메시지 클래스 수정

--- backend/src/main/java/com/onion/backend/controller/ChatController.java
+++ backend/src/main/java/com/onion/backend/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.onion.backend.controller;
 
 import com.onion.backend.dto.chat.ChatMessage;
+import java.util.Date;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -11,9 +12,7 @@ public class ChatController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public ChatMessage greeting(ChatMessage chatMessage) throws Exception {
-        Thread.sleep(1000); // simulated
-        System.out.println("chatMessage = " + chatMessage);
-        return new ChatMessage("Hello, " + chatMessage.getMessage() + "!");
+        return chatMessage;
     }
 
 }
