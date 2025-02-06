5a35dc968cd7c65efa9362d14fdc84f999f45a6c
seungheon <seungheon@apexsoft.co.kr>
2025-02-07 00:49:18 +0900

    댓글 엔티티 repository querydsl 구현체 생성

---
+++ backend/src/main/java/com/onion/backend/repository/comment/CommentRepositoryImpl.java
@@ -0,0 +1,25 @@
+package com.onion.backend.repository.comment;
+
+import static com.onion.backend.entity.QComment.comment;
+
+import com.onion.backend.entity.Comment;
+import com.onion.backend.entity.QComment;
+import com.onion.backend.entity.User;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+public class CommentRepositoryImpl extends QuerydslRepositorySupport implements CommentRepositoryCustom {
+
+    public CommentRepositoryImpl() {
+        super(Comment.class);
+    }
+
+
+    @Override
+    public Comment getLatestCreateCommentByAuthor(User user) {
+        return from(comment)
+            .where(comment.user.eq(user))
+            .orderBy(comment.createdAt.desc())
+            .limit(1)
+            .fetchOne();
+    }
+}
