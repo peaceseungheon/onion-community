5248b1e5f0a091cdf2d912b71e2b5c38e7fccaf4
seungheon <seungheon@apexsoft.co.kr>
2025-02-18 22:24:22 +0900

    REST API 원칙에 맞게 복수형으로 수정

--- backend/src/main/java/com/onion/backend/controller/CommentController.java
+++ backend/src/main/java/com/onion/backend/controller/CommentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/boards/{boardId}/articles/{articleId}/comment")
+@RequestMapping("/api/boards/{boardId}/articles/{articleId}/comments")
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
